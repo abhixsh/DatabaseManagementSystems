bc9fdb8a71c244e20493503081dec6f446f03080
‘abhixsh’ <aloka.abishek9@gmail.com>
2023-05-14 19:51:11 +0530

    1st practile execrises

---
+++ Practicle01.sql
@@ -0,0 +1,32 @@
+create database Faculty;
+CREATE DATABASE University;
+
+SHOW DATABASES;
+
+USE Faculty;
+
+create TABLE <staff> (
+    <ID><INT>
+    <FirstName><VARCHAR(20)>
+    <LastName><VARCHAR(20)>
+    <City><VARCHAR(20)>
+    <Telephone><VARCHAR(20)>
+    <SubjectCode><VARCHAR(20)>
+)
+
+create TABLE <BBSTStudents> (
+    <ID><INT>
+    <Surename><VARCHAR(20)>
+    <City><VARCHAR(20)>
+    <Email><VARCHAR(20)>
+    <GPA><VARCHAR(20)>
+)
+
+Show TABLE;
+
+DESCRIBE <staff>;
+DESCRIBE <BBSTStudents>;
+
+DROP TABLE <staff>;
+
+Show TABLE;
