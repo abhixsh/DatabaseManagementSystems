c9016ed3af8c3b182dbd0a6dca0cc178d3501753
‘abhixsh’ <aloka.abishek9@gmail.com>
2023-06-12 11:00:18 +0530

    prac 06 created

---
+++ Practicle06.sql
@@ -0,0 +1,17 @@
+create database Prcticle06;
+
+USE Prcticle06;
+
+CREATE TABLE PRODUCTS(
+    productID INT,
+    ProductName VARCHAR(20),
+    Price INT
+);
+
+INSERT INTO PRODUCT(productID, ProductName, Price )
+VALUES
+(1,"Laptop",1000),
+(2,"Smartphone",800),
+(3,"Headphone",50),
+(4,"Keybord",70),
+(5,"Mouse",20);
