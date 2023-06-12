9afeb0251f81b82b96bb2b0c4d0f691ebb5eee09
‘abhixsh’ <aloka.abishek9@gmail.com>
2023-06-12 12:04:20 +0530

    6/12 ex

--- Practicle06.sql
+++ Practicle06.sql
@@ -8,10 +8,64 @@ CREATE TABLE PRODUCTS(
     Price INT
 );
 
-INSERT INTO PRODUCT(productID, ProductName, Price )
+INSERT INTO PRODUCTS(productID, ProductName, Price )
 VALUES
 (1,"Laptop",1000),
 (2,"Smartphone",800),
 (3,"Headphone",50),
 (4,"Keybord",70),
 (5,"Mouse",20);
+
+SELECT * FROM PRODUCTS
+WHERE Price BETWEEN 50 AND 100;
+
+SELECT * FROM PRODUCTS
+WHERE Price NOT BETWEEN 10 AND 20;
+
+SELECT * FROM PRODUCTS
+WHERE Price NOT BETWEEN 50 AND 100;
+
+SELECT * FROM PRODUCTS
+WHERE Price NOT BETWEEN 10 AND 20
+AND productID NOT IN(1,2,3);
+
+SELECT * FROM PRODUCTS
+WHERE Price  BETWEEN 50 AND 100
+OR Price  IN(1000,20);
+
+SELECT * FROM PRODUCTS
+WHERE Price IN (800,20);
+
+SELECT * FROM PRODUCTS
+WHERE ProductName IN ('Laptop','Headphone','Mouse' );
+
+SELECT * FROM PRODUCTS
+WHERE Price IN (SELECT Price FROM PRODUCTS WHERE Price > 800 )
+OR Price in (SELECT Price FROM PRODUCTS WHERE Price < 50);
+
+SELECT ProductName, Price FROM PRODUCTS
+WHERE Price > (SELECT AVG (Price) FROM PRODUCTS );
+
+SELECT * FROM PRODUCTS
+WHERE ProductName LIKE '%phone%';
+
+SELECT * FROM PRODUCTS
+WHERE ProductName LIKE 'L%';
+
+SELECT * FROM PRODUCTS
+WHERE ProductName LIKE '%E';
+
+SELECT * FROM PRODUCTS
+WHERE ProductName LIKE '%p%';
+
+SELECT * FROM PRODUCTS
+WHERE ProductName LIKE '______';
+
+INSERT INTO PRODUCTS(productID, ProductName, Price )
+VALUES
+(6,"Monitor",450),
+(7,"Tablet",600),
+(8,"Printer",300),
+(9,"Camera",750),
+(10,"Speaker",120);
+
